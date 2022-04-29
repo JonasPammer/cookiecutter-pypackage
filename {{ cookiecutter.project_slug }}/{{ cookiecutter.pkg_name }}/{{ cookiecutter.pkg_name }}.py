8c6d9e114ab3641508ddb8023e5879dec3c1cd34
Jonas Pammer <opensource@jonaspammer.at>
2022-04-29 13:35:48 +0200

    fix: add typing to main python file skeleton

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+from typing import Sequence
+
 import argparse
 {%- endif %}
 {% if cookiecutter.command_line_interface|lower == 'click' %}
@@ -9,11 +11,11 @@ import click
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
-def main(args=None):
+def main() -> int:
     return 0
 {%- endif %}
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
-def main():
+def main(argv: Sequence[str] | None = None):
     parser = argparse.ArgumentParser()
     parser.add_argument('_', nargs='*')
     args = parser.parse_args()
