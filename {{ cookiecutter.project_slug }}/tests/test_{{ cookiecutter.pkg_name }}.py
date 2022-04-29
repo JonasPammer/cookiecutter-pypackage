4aa1f4d60d7aa55873506990c2353625b70e1bcc
Jonas Pammer <opensource@jonaspammer.at>
2022-04-29 12:39:24 +0200

    chore: add skeleton tests and main project python file
    source https://github.com/audreyfeldroy/cookiecutter-pypackage/tree/7696a30f4fb66c6c87815c51e12e7c7e3985e844

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.pkg_name }}.py
@@ -0,0 +1,62 @@
+"""Tests for `{{ cookiecutter.project_slug }}` package."""
+
+{% if cookiecutter.use_pytest == 'y' -%}
+import pytest
+{% else %}
+import unittest
+{%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+from click.testing import CliRunner
+{%- endif %}
+
+from {{ cookiecutter.pkg_name }} import {{ cookiecutter.pkg_name }}
+
+
+{%- if cookiecutter.use_pytest == 'y' %}
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+{%-   if cookiecutter.command_line_interface|lower == 'click' %}
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke({{ cookiecutter.pkg_name }}.main)
+    assert result.exit_code == 0
+    # assert 'some string' in result.output
+{%-   endif %}
+{%- else %}
+
+
+class Test{{ cookiecutter.pkg_name|title }}(unittest.TestCase):
+    """Tests for `{{ cookiecutter.pkg_name }}` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke({{ cookiecutter.pkg_name }}.main)
+        assert result.exit_code == 0
+    # assert 'some string' in result.output
+{%- endif %}
+{%- endif %}
---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+import argparse
+{%- endif %}
+import sys
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+import click
+{%- endif %}
+
+{% if cookiecutter.command_line_interface|lower == 'click' %}
+@click.command()
+def main(args=None):
+    return 0
+{%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+    return 0
+{%- endif %}
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
