41afe8f7ed1379585f7e362bd8f2ab0e06662bee
Jonas Pammer <opensource@jonaspammer.at>
2022-07-22 15:01:56 +0200

    style: prettier²

--- {{ cookiecutter.project_slug }}/setup.py
+++ {{ cookiecutter.project_slug }}/setup.py
@@ -1,10 +1,10 @@
 """
-
-> If compatibility with legacy builds
-> (those not using the PEP 517 build API, i.e. pip <19) is desired,
-> a setup.py file containing a setup() function call is still required
-> even if your configuration resides in setup.cfg.
 https://setuptools.pypa.io/en/latest/userguide/declarative_config.html
+
+If compatibility with legacy builds
+(those not using the PEP 517 build API, i.e. pip <19) is desired,
+a setup.py file containing a setup() function call is still required
+even if your configuration resides in setup.cfg.
 """
 from __future__ import annotations
 
