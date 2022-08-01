e71d270376969d3eb0c3155c24284b240a6a5997
Jonas Pammer <opensource@jonaspammer.at>
2022-08-01 11:06:53 +0200

    refactor: implement changes from upstream PR
    i accidentally copied an old version of docker_smoke_test.py beforehand. the other 2 PR changes i copied directly from github ui so they are up to date

--- {{ cookiecutter.project_slug }}/docker_smoke_test.py
+++ {{ cookiecutter.project_slug }}/docker_smoke_test.py
@@ -1,6 +1,11 @@
-"""https://pythonspeed.com/articles/test-your-docker-build/"""
+"""Pure, Dependency-Free Python3 File to 'Smoke Test' running the Docker
+Container.
+
+https://pythonspeed.com/articles/test-your-docker-build/
+"""
 from __future__ import annotations
 
+import sys
 from subprocess import CalledProcessError
 from subprocess import check_call
 from subprocess import check_output
@@ -10,17 +15,28 @@ from time import sleep
 
 def _print(_: str) -> None:
     print(f"[{__file__}] {_}")
+    sys.stdout.flush()
 
 
-def _get_docker_logs(docker_container_id: str) -> str:
-    return check_output(["docker", "logs", "docker_container_id"]).decode("utf-8")
+def get_docker_logs(docker_container_id: str) -> str:
+    return check_output(["docker", "logs", f"{docker_container_id}"]).decode("utf-8")
+
+
+def _log_docker_logs(docker_container_id: str) -> None:
+    _preamble: str = (
+        "-" * 6 + f" DOCKER CONTAINER LOGS OF '{docker_container_id}' " + "-" * 6
+    )
+    _print(_preamble)
+    print(get_docker_logs(docker_container_id))
+    _print("-" * int(len(_preamble) / 2))
 
 
 def main() -> int:
     _print("Building local version of Docker Image...")
     check_call(
         "docker buildx build . "
-        "--tag {{ cookiecutter.dockerhub_username }}/{{ cookiecutter.project_slug }}:smoke-please-ignore".split()
+        "--tag {{ cookiecutter.dockerhub_username }}/{{ cookiecutter.project_slug }}:smoke-please-ignore "
+        "--load".split()
     )
 
     # FIXME: adjust run command here as needed
@@ -42,7 +58,7 @@ def main() -> int:
         _print("FIXME: Implement actual Smoke Tests here if any!")
 
         # Example 1: Check output
-        # assert "xyz" in _get_docker_logs(docker_container_id)
+        # assert "xyz" in get_docker_logs(docker_container_id)
 
         # Example 2: Check if a http server started
         # (urlopen throws an URLError on http protocol errors):
@@ -64,20 +80,22 @@ def main() -> int:
         if docker_container_entrypoint_exit_code != "0":
             # raise right here so container is not killed/removed
             # in order to allow for debugging
+            _log_docker_logs(docker_container_id)
             raise Exception(
                 f"Docker Container '{docker_container_id}' exited with "
-                f"{docker_container_entrypoint_exit_code}"
+                f"{docker_container_entrypoint_exit_code}."
             )
 
         try:
             _print("Killing Container...")
             check_output(f"docker kill {docker_container_id}".split(), stderr=STDOUT)
             # FIXME: remove the below exception catch if the dockerfile
-            #        actually shouldve actually started a server
+            #        shouldve actually started a server
         except CalledProcessError as ex:
             if "not running" not in ex.output.decode():
                 raise ex
         finally:
+            _log_docker_logs(docker_container_id)
             _print("Removing Container...")
             check_call(f"docker rm {docker_container_id}".split())
     return 0
